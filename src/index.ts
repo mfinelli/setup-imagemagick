002aebfc8a3daaea0f6304df541baf3b6882c972
Mario Finelli <mario@finel.li>
2021-03-13 14:19:49 -0500

    Fix prettier style

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,34 @@
-import * as core from '@actions/core'
-import * as exec from '@actions/exec'
-import * as io from '@actions/io'
-import * as os from 'os'
-import * as path from 'path'
-import * as tc from '@actions/tool-cache'
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as io from "@actions/io";
+import * as os from "os";
+import * as path from "path";
+import * as tc from "@actions/tool-cache";
 
-const LINUX_BIN = 'https://download.imagemagick.org/ImageMagick/download/binaries/magick'
+const LINUX_BIN =
+  "https://download.imagemagick.org/ImageMagick/download/binaries/magick";
 
 async function run(): Promise<void> {
   try {
-    if (process.platform === 'win32') {
+    if (process.platform === "win32") {
       // todo: url changes as new versions are released and old versions do not
       //       remain available
-      core.setFailed('Not currently supported on windows runners')
-      return
-    } else if(process.platform === 'darwin') {
+      core.setFailed("Not currently supported on windows runners");
+    } else if (process.platform === "darwin") {
       // todo: homebrew install imagemagick
-      core.setFailed('Not currently supported on macos runners')
-      return
+      core.setFailed("Not currently supported on macos runners");
     } else {
-      const binPath = `${os.homedir}/bin`
-      await io.mkdirP(binPath)
-      const magickPath = await tc.downloadTool(LINUX_BIN)
-      await io.mv(magickPath, `${binPath}/magick`)
-      exec.exec('chmod', ['+x', `${binPath}/magick`])
+      const binPath = `${os.homedir}/bin`;
+      await io.mkdirP(binPath);
+      const magickPath = await tc.downloadTool(LINUX_BIN);
+      await io.mv(magickPath, `${binPath}/magick`);
+      exec.exec("chmod", ["+x", `${binPath}/magick`]);
 
-      core.addPath(binPath)
+      core.addPath(binPath);
     }
-  } catch(error) {
-    core.setFailed(error.message)
+  } catch (error) {
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
