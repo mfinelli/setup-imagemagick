dabaf448f563e7d33a11e06d1b35443a7811341d
Mario Finelli <mario@finel.li>
2023-09-18 22:47:35 +0200

    Wait for apt-get to finish

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ async function run(): Promise<void> {
     } else {
       let installLibfuse2 = core.getBooleanInput("install-libfuse2");
       if (installLibfuse2) {
-        exec.exec("sudo", ["apt-get", "install", "-y", "libfuse2"]);
+        await exec.exec("sudo", ["apt-get", "install", "-y", "libfuse2"]);
       }
 
       const binPath = `${os.homedir}/bin`;
